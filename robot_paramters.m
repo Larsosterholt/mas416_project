9be04f95892d8772714ff33e10e44f0b782e85d6
LarsO <you@example.com>
2023-10-23 13:28:36 +0200

    Started simscape model

---
+++ robot_paramters.m
@@ -0,0 +1,21 @@
+clc; clear;
+g = 9.81;
+L = [240 800 1050 1175 225 200 235 248]/1000;
+T = 0.1;
+PB = T+0.02;
+
+% Paramters for blue base
+m1 = 750; % kg
+I1 = [50 50 50]; % Inertia kg*m^2
+
+% Parameters for red rod
+m3 = 250; % kg
+I3 = [1 20 20]; % kg*m^2
+
+% Parameters for grenn rod and mass
+m4 = 150; % kg
+I4 = [0.5 0.8 8]; % kg*m^2
+
+m5 = 500; % kg
+I5 = [10 10 10]; % kg*m^2
+
