017926c242cca6dccf8c6c46581a40f4c3ed709a
LarsO <you@example.com>
2023-10-25 14:06:02 +0200

    Cleaned up un model

--- robot_paramters.m
+++ robot_paramters.m
@@ -2,7 +2,6 @@ clc; clear;
 g = 9.81;
 L = [240 800 1050 1175 225 200 235 248]/1000;
 T = 0.1;
-PB = T+0.02;
 
 % Paramters for blue base
 m1 = 750; % kg
@@ -15,22 +14,26 @@ I3 = [1 20 20]; % kg*m^2
 % Parameters for grenn rod and mass
 m4 = 150; % kg
 I4 = [0.5 0.8 8]; % kg*m^2
-
 m5 = 500; % kg
 I5 = [10 10 10]; % kg*m^2
 
+% White rod
 m6 = 50; % kg
 I6 = [0.2, 1, 1]; % kg*m^2
 
+% Blue
 m7 = 150; % kg
 I7 = [2 3 2];
 
+% Grey rod
 m8 = 200;
 I8 = [1 18 18];
 
+% Black rod
 m9 = 100;
 I9 = [0.5 2 2];
 
+% EE
 m10 = 50;
-I10 = [1 1 1]
+I10 = [1 1 1];
 
